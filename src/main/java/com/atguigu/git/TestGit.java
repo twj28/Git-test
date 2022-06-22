a3a76f43d40dcc599fdecb85c70bddb7eee820f5
twj280 <11356119@qq.com>
2022-06-23 00:44:03 +0800

    first commit

---
+++ src/main/java/com/atguigu/git/TestGit.java
@@ -0,0 +1,15 @@
+package com.atguigu.git;
+
+/**
+ * @ClassNmae TestGit
+ * @Description TODO
+ * @Author twj280
+ * @Date 2022/6/23 0:41
+ * @Version 1.0
+ **/
+public class TestGit {
+    public static void main(String[] args) {
+        System.out.println("hello,idea git1!");
+
+    }
+}
